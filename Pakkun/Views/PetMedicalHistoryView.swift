c60c6a7e31f1ccd2c514b3fdca75c6c52fb3a2cd
Alejandra Guevara <alejandra.gue91@gmail.com>
2025-01-27 16:00:53 -0500

    Pet Medical History UI in progress

---
+++ Pakkun/Views/PetMedicalHistoryView.swift
@@ -0,0 +1,22 @@
+//
+//  MedicalView.swift
+//  Pakkun
+//
+//  Created by New Student on 1/24/25.
+//
+
+import SwiftUI
+
+struct RegisterPetMedicalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct RegisterPetMedicalView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterPetMedicalView()
+        }
+    }
+
